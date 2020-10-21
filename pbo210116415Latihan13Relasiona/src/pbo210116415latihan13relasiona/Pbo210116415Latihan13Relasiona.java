d499875d5986a91e30103f2345ca3f6694b834bc
frmia18 <50294841+frmia18@users.noreply.github.com>
2020-10-21 09:49:28 +0700

    Add files via upload

---
+++ pbo210116415Latihan13Relasiona/src/pbo210116415latihan13relasiona/Pbo210116415Latihan13Relasiona.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo210116415latihan13relasiona;
+
+/**
+ *
+ * @author Frda
+ */
+public class Pbo210116415Latihan13Relasiona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+             int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
